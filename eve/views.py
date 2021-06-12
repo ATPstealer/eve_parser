010ee0e96c0207090e25de3ff7907a507255e31c
Mav <email@amulin.ru>
2021-06-12 16:49:55 +0300

    init

---
+++ eve/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+import datetime
+
+
+def hello(request):
+    return HttpResponse("Hello world")
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
