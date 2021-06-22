986070db5ed52008277ee5eb71cb3ef64ba32196
Mav <email@amulin.ru>
2021-06-22 09:49:29 +0300

    Market History Parser

--- eve_parser/include/parser.py
+++ eve_parser/include/parser.py
@@ -1,5 +1,6 @@
 from config import Config
 import requests
+import time
 
 
 class Parser:
@@ -11,9 +12,15 @@ class Parser:
         get_args = ""
         for key in dict_get_args:
             get_args += "&" + key + "=" + str(dict_get_args[key])
-        try:
-            r = requests.get(self.config.esi + section + self.config.server + get_args)
-        except requests.exceptions.RequestException as e:
-            print("request can't receive data: %s" % e)
-        else:
-            return r.text
+        for k in range(1, 4):
+            try:
+                r = requests.get(self.config.esi + section + self.config.server + get_args)
+            except requests.exceptions.RequestException as e:
+                print("request can't receive data: %s" % e)
+            else:
+                if r.status_code == 200 or r.status_code == 404:
+                    return r.text
+                else:
+                    print("Response code: " + str(r.status_code))
+                    time.sleep(5)
+        return r.text
--- eve_parser/scripts/parser_market_history.py
+++ eve_parser/scripts/parser_market_history.py
@@ -13,16 +13,16 @@ def run():
             market_history_json = parser.evetech_req("/markets/" + str(region[0]) + "/history/", dict_get_args)
             if "error" in market_history_json:
                 continue
+            print(market_history_json)
             insert_in_base(json.loads(market_history_json), region[0], item_type[0])
     print("start at: %s\nend at: %s" % (start, datetime.now()))
 
 
 def insert_in_base(market_history_data, region, item_type):
+    print("Item: " + str(region) + " " + str(item_type))
     for market_history_day in market_history_data:
         m = list(MarketHistory.objects.filter(region_id=region, type_id=item_type, date=market_history_day['date']))
-        print("Skip: " + str(region) + " " + str(item_type) + " " + market_history_day['date'])
         if len(m) == 0:
-            print("Create: " + str(region) + " " + str(item_type) + " " + market_history_day['date'])
             market_history = MarketHistory.objects.create(
                 region_id=region, type_id=item_type,
                 date=market_history_day['date'], average=market_history_day['average'],
