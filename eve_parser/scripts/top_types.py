7cd919e22d4b0ecb6943511eeb296f9690ecc20e
andmulin <email@amulin.ru>
2021-07-04 23:25:45 +0300

    top types new

--- eve_parser/scripts/top_types.py
+++ eve_parser/scripts/top_types.py
@@ -1,9 +1,15 @@
-from eve_parser.models import Liquidity, TopTypes
+from eve_parser.models import Liquidity, TopTypes, Types
+from django.db.models import Q
 
 
 def run():
     TopTypes.objects.all().delete()
-    liq = Liquidity.objects.filter(region_id=10000002, month_volume__gte=0.001)
-    for t in liq:
-        types = TopTypes.objects.create(type_id=t.type_id)
-        types.save()
+    liquidity = Liquidity.objects.filter(region_id=10000002, day_volume__gte=0.001)
+    types = Types.objects.filter(~Q(market_group_id=0), ~Q(name__contains="blueprint"), ~Q(name__contains="SKIN"),
+                                 ~Q(name__contains="Men's"), ~Q(name__contains="Women's"))
+    for typ in types:
+        print(typ.type_id)
+        for liq in liquidity:
+            if liq.type_id == typ.type_id:
+                types = TopTypes.objects.create(type_id=liq.type_id)
+                types.save()
