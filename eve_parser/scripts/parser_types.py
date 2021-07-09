6ad0f1d043e9185ca230ec7afd9e71255aead1f7
andmulin <email@amulin.ru>
2021-07-09 17:40:40 +0300

    return liq calc

--- eve_parser/scripts/parser_types.py
+++ eve_parser/scripts/parser_types.py
@@ -64,12 +64,12 @@ def clear_types():
 
 def top_types():
     TopTypes.objects.all().delete()
-    # liquidity = Liquidity.objects.filter(region_id=10000002, day_volume__gte=0.001)
-    types = Types.objects.filter(~Q(market_group_id=0), ~Q(name__contains="blueprint"), ~Q(name__contains="SKIN"),
-                                 ~Q(name__contains="Men's"), ~Q(name__contains="Women's"))
+    liquidity = Liquidity.objects.filter(region_id=10000002, day_volume__gte=0.001)
+    types = Types.objects.filter(~Q(market_group_id=0), ~Q(name__contains="blueprint"), ~Q(name__contains="Blueprint"),
+                                 ~Q(name__contains="SKIN"), ~Q(name__contains="Men's"), ~Q(name__contains="Women's"))
     for typ in types:
         print(typ.type_id)
-        # for liq in liquidity:
-            # if liq.type_id == typ.type_id:
-        types_for_save = TopTypes.objects.create(type_id=typ.type_id, name=typ.name)
-        types_for_save.save()
+        for liq in liquidity:
+            if liq.type_id == typ.type_id:
+               types_for_save = TopTypes.objects.create(type_id=typ.type_id, name=typ.name)
+               types_for_save.save()
