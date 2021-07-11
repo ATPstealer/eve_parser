acdbe9b23446e6ae6b27cab5acd8bc3a9f1afa70
andmulin <email@amulin.ru>
2021-07-11 17:50:17 +0300

    liquidity page

--- logistics/urls.py
+++ logistics/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.liquidity, name='liquidity'),
+    path('liquidity', views.liquidity, name='liquidity'),
+    path('planing', views.planing, name='liquidity'),
 ]
--- logistics/views.py
+++ logistics/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from eve_parser.models import Liquidity, Types
+from eve_parser.models import Liquidity, Types, Regions
+from logistics.models import models
 
 
 def liquidity(request):
-    region_id = request.GET["region_id"]
+    if "region" not in request.GET:
+        region = "The Forge"
+    else:
+        region = request.GET["region"]
+    try:
+        region_id = Regions.objects.get(name=str(region)).region_id
+    except models.ObjectDoesNotExist as e:
+        region_id = 10000002
+        region = "The Forge"
+
     liquidity_array = Liquidity.objects.values_list("type_id", "day_volume", "day_turnover", "price").\
         filter(region_id=int(region_id)).order_by("-day_turnover")
     print(liquidity_array)
@@ -14,9 +24,13 @@ def liquidity(request):
         if count_display > 100:
             break
         name = Types.objects.values_list("name").filter(type_id=liq[0])
-        liquidity_to_page.append({'name': name[0][0], 'type_id': liq[0], 'day_volume': liq[1], 'day_turnover': liq[2], 'price': liq[3]})
+        liquidity_to_page.append({'name': name[0][0], 'type_id': liq[0], 'day_volume': liq[1],
+                                  'day_turnover': liq[2], 'price': liq[3]})
 
-    print(liquidity_to_page)
+    regions = Regions.objects.values_list("name")
+    return render(request, 'logistics/liquidity.html',
+                  context={'region_selected': region, 'regions': regions, 'liquidity': liquidity_to_page})
 
-    return render(request, 'logistics/liquidity.html', context={'region_id': region_id, 'liquidity': liquidity_to_page})
 
+def planing(request):
+    return render(request, 'logistics/liquidity.html', context={})
