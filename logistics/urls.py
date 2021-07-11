25ee8ed29f386272caabdb700751ff3b46304db9
andmulin <email@amulin.ru>
2021-07-11 17:55:58 +0300

    liquidity page

--- logistics/urls.py
+++ logistics/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('liquidity', views.liquidity, name='liquidity'),
-    path('planing', views.planing, name='liquidity'),
+    path('planing', views.planing, name='planing'),
 ]
