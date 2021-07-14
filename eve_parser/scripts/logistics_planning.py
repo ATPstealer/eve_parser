960f6a098c5ea05206363515b0c7864e910e27d6
andmulin <email@amulin.ru>
2021-07-14 21:36:36 +0300

    New Parser status

--- eve_parser/scripts/logistics_planning.py
+++ eve_parser/scripts/logistics_planning.py
@@ -67,6 +67,7 @@ def calculate_logistics(region_from, region_to, day_turnover_threshold):
 
 
 def check_need(region_from, region_to):
+    print(region_from, region_to)
     liq_to = ParserDateStatus.objects.get(parser_name="Liquidity calculation", region_id=region_to)
     print(liq_to.parse_time)
     log = ParserDateStatus.objects.get(parser_name="Calculate logistic", region_id=region_from, region_id_log=region_to)
