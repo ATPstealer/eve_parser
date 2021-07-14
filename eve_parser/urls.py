2ed0494aee20ecbb5d7cb7ffdd41630eb70119d1
andmulin <email@amulin.ru>
2021-07-14 21:59:48 +0300

    New Parser status

--- eve_parser/urls.py
+++ eve_parser/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('stats', views.stats, name='stats'),
+    path('stats2', views.stats2, name='stats2'),
 ]
--- eve_parser/views.py
+++ eve_parser/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from eve_parser.models import ParserStatus
+from eve_parser.models import ParserStatus, ParserDateStatus
 
 
 def stats(request):
-    p = ParserStatus.objects.values_list("name", "describe", "region_id", "now_parse")
+    p = ParserDateStatus.objects.values_list("parser_name", "region_id", "region_id_log", "parse_time")
     return render(request, 'eve_parser/stats.html', context={'statistics': p})
+
+
+def stats2(request):
+    p = ParserStatus.objects.values_list("name", "describe", "region_id", "now_parse")
+    return render(request, 'eve_parser/stats2.html', context={'statistics': p})
 
 
 
