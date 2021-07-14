b4c9ac1d5cc5d80ee4f4b9cb62bd6b5d3912d4bb
andmulin <email@amulin.ru>
2021-07-14 21:47:37 +0300

    New Parser status

--- eve_parser/scripts/logistics_planning.py
+++ eve_parser/scripts/logistics_planning.py
@@ -17,7 +17,6 @@ def run(*args):
         for reg_to in region_to:
             if int(reg_from[0]) < int(reg_to[0]):
                 calculate_logistics(reg_from[0], reg_to[0], day_turnover_threshold)
-            Parser.parser_date_status("Calculate logistic", reg_from[0], reg_to[0])
     print("start at: %s\n end at: %s" % (start, datetime.now()))
     Parser.parser_status("Calculate logistics", "Done \nStart at: %s\n end at: %s" % (start, datetime.now()), 0, 0)
 
@@ -64,6 +63,7 @@ def calculate_logistics(region_from, region_to, day_turnover_threshold):
                     day_volume_from=liquidity_from.day_volume, day_volume_to=liquidity_to.day_volume,
                     profit_from=float((liquidity_from.price - liquidity_to.price) * (-1) * liquidity_to.day_volume) / 1000000,
                     profit_to=float((liquidity_from.price - liquidity_to.price) * liquidity_from.day_volume) / 1000000)
+    Parser.parser_date_status("Calculate logistic", region_from, region_to)
 
 
 def check_need(region_from, region_to):
