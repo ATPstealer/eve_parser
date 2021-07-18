ae1828167875cb978d5168251730f0db8dbaf845
andmulin <email@amulin.ru>
2021-07-18 11:04:34 +0300

    Liquidity calc correct

--- eve_parser/scripts/liquidity.py
+++ eve_parser/scripts/liquidity.py
@@ -30,7 +30,7 @@ def liquidity_calc(region_id, type_id, month_ago, market_history):
     day_range = timedelta(0)
     average_price_3_days = 0
     parser_date_status = ParserDateStatus.objects.filter(parser_name="Market history", region_id=region_id)
-    print(parser_date_status[0].parse_time.date()-month_ago)
+    print(parser_date_status[0].parse_time.date()-month_ago.date())
     for market_history_day in market_history:
         days += 1
         volume += market_history_day.volume
