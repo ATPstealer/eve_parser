9693b3c515778ca91f1e70cd506075f5df30a47a
andmulin <email@amulin.ru>
2021-07-23 12:19:03 +0300

    neural network app

--- neural_network/scripts/test.py
+++ neural_network/scripts/test.py
@@ -21,6 +21,19 @@ test_data -= mean
 test_data /= std
 print(test_data)
 
+k = 4
+num_val_samples = len(train_data) // k
+num_epochs = 100
+all_scores = []
+for i in range(k):
+    print('processing fold #', i)
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+    partial_train_data = np.concatenate( [train_data[:i * num_val_samples], train_data[(i + 1) * num_val_samples:]], axis=0)
+    partial_train_targets = np.concatenate( [train_targets[:i * num_val_samples], train_targets[(i + 1) * num_val_samples:]], axis=0)
+    model = build_model()
+
+
 def build_model():
     model = models.Sequential()
     model.add(layers.Dense(64, activation='relu',input_shape=(train_data.shape[1],)))
