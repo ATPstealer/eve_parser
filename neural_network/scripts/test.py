5251350c5839a910be9acacd4e604511363a6aa2
andmulin <email@amulin.ru>
2021-07-23 19:01:54 +0300

    neural network app

--- neural_network/scripts/test.py
+++ neural_network/scripts/test.py
@@ -40,7 +40,7 @@ for i in range(k):
     model = build_model()
 
 
-model.fit(partial_train_data, partial_train_targets, epochs=num_epochs, batch_size=1, verbose=0)
+model.fit(partial_train_data, partial_train_targets, epochs=num_epochs, batch_size=1)
 val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
 all_scores.append(val_mae)
 
@@ -49,7 +49,7 @@ print(np.mean(all_scores))
 
 num_epochs = 500
 
-model.fit(partial_train_data, partial_train_targets, epochs=num_epochs, batch_size=1, verbose=0)
+model.fit(partial_train_data, partial_train_targets, epochs=num_epochs, batch_size=1)
 val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
 all_scores.append(val_mae)
 
