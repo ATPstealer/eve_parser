f6d5b06d8c5d68f3953ea52a7385ba4875a86c7f
andmulin <email@amulin.ru>
2021-07-23 11:58:56 +0300

    neural network app

--- neural_network/scripts/test.py
+++ neural_network/scripts/test.py
@@ -1,7 +1,25 @@
 from keras.datasets import boston_housing
-
+from keras import models
+from keras import layers
 
 (train_data, train_targets), (test_data, test_targets) = boston_housing.load_data()
 
 print(train_data.shape)
 print(test_data.shape)
+print(test_data)
+
+mean = train_data.mean(axis=0)
+train_data -= mean
+std = train_data.std(axis=0)
+train_data /= std
+test_data -= mean
+test_data /= std
+
+
+def build_model():
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation='relu',input_shape=(train_data.shape[1],)))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(1))
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    return model
