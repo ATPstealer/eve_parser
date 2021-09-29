de64d3ac5cc9b1e40296ff1ccb8877f416637417
ATPstealer <test-test@gmail.com>
2021-09-29 21:56:22 +0300

    config

--- eve/settings.py
+++ eve/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = eve.settings_secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = eve.settings_secrets.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = eve.settings_secrets.ALLOWED_HOSTS
 
 
 # Application definition
